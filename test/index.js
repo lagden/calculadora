ad91950b424c4e94f8947a6da853c36be3fecae8
Thiago Lagden <lagden@gmail.com>
2016-11-04 01:44:43 -0200

    feito os testes dos modulos de soma e subtracao

--- test/index.js
+++ test/index.js
@@ -1,7 +1,15 @@
 'use strict'
 
 import test from 'ava'
+import soma from '../app/modules/soma'
+import subtracao from '../app/modules/subtracao'
 
-test('empty', t => {
-	t.true(true)
+test('soma', t => {
+	const total = soma(2, 3, 4)
+	t.is(total, 9)
+})
+
+test('subtracao', t => {
+	const total = subtracao(2, 3, 4)
+	t.is(total, -5)
 })
